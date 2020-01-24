d4faa78af4b78726b5f20753594fee523e4b7af0
mindtree <mitchell.nordine@gmail.com>
2020-01-24 20:18:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# mindtree NixOS setup
+
+Clone into the nix config directory.
+
+```
+cd /etc/nixos/
+git clone git@github.com:mitchmindtree/nixconf.git mindtree
+```
+
+Add `mindtree/conf.nix` to the imports within `/etc/nixos/configuration.nix`
---
+++ conf.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+{
+  # Set your time zone.
+  time.timeZone = "Europe/Berlin";
+
+  # Packages.
+  environment.systemPackages = with pkgs; [
+    firefox
+    tmux
+    wget
+    vim
+  ];
+
+  # Networking.
+  networking.hostName = "mindtree";
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
+
+  # Enable touchpad support.
+  # services.xserver.libinput.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+}
