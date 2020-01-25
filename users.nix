4ac6b5dd3bd4d07df8c522317aa9e815f7e5439e
mindtree <mitchell.nordine@gmail.com>
2020-01-25 15:03:28 +0100

    Include users.nix in nixconf repo

---
+++ users.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+{
+  # Ensure the users on the system match the set in `users.users`.
+  users.mutableUsers = false;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mindtree = {
+    isNormalUser = true;
+    home = "/home/mindtree";
+    description = "mindtree";
+    hashedPassword = "$6$mindtree$VYHsIVYocBAFdtWXiZLX2snVDqBGjv9mbJylf5RDMjm3cTALnk1hMyMsdmYET8FzffBlcyqOA.BoqEYFdmjum.";
+    openssh.authorizedKeys.keys = [];
+    extraGroups = [
+      "wheel"
+      "networkmanager"
+      # "storage"
+      # "power"
+      # "plugdev"
+      # "wireshark"
+      # "vboxusers"
+    ];
+  };
+}
