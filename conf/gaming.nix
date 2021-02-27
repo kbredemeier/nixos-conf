fbdfe14b8f68a85eaf5a02931697fe47c33bdc97
mitchmindtree <mail@mitchellnordine.com>
2021-02-27 14:37:57 +0100

    Move games into home-manager. Enable steam via programs.

--- conf/gaming.nix
+++ conf/gaming.nix
@@ -1,22 +1,20 @@
 { config, pkgs, ... }:
 {
-  # Packages.
-  environment.systemPackages = with pkgs; [
-    cataclysm-dda
-    minecraft
-    steam
-  ];
-
   # Steam requires unfree.
   # TODO: Work out how to make this specifically just steam.
   nixpkgs.config.allowUnfree = true;
 
   # Steam requirements.
-  hardware.opengl = {
-    driSupport = true;
-    driSupport32Bit = true;
-    extraPackages = with pkgs; [ libva ];
-    extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware = {
+    opengl = {
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages = with pkgs; [ libva ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+    };
+    pulseaudio.support32Bit = true;
   };
-  hardware.pulseaudio.support32Bit = true;
+
+  # Includes the package along with "a few other workarounds" like controller support.
+  programs.steam.enable = true;
 }
