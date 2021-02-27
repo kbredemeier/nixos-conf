0e9ca7354e4f402c074a198ad20b641fd46c3777
mitchmindtree <mail@mitchellnordine.com>
2021-02-27 14:39:31 +0100

    Increase tmux history limit

--- home/tmux.nix
+++ home/tmux.nix
@@ -3,7 +3,7 @@
   programs.tmux = {
     enable = true;
     keyMode = "vi";
-    historyLimit = 10000;
+    historyLimit = 100000;
     extraConfig = ''
       # Set the tmux status bar color
       set -g default-terminal "screen-256color"
