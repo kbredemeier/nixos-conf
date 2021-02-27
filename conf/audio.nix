b87866ce6ff9043d8714cd451f6fb0cf6524d68b
mitchmindtree <mail@mitchellnordine.com>
2021-02-27 14:37:00 +0100

    Remove unused audio tools

--- conf/audio.nix
+++ conf/audio.nix
@@ -4,31 +4,15 @@
   # Packages.
   environment.systemPackages = with pkgs; [
     ardour
-    cadence # Trying out, alternative to qjackctl it seems?
     mixxx # DJ mixer
-    pamixer # pulseaudio cli mixer
-    pavucontrol # pulseaudio mixer
-    qjackctl # Trying to start jack server from user.
-    reaper
     supercollider
     yoshimi # Plugin
     zam-plugins # Some random plugins I'm about to try
     zynaddsubfx # Plugin
   ];
 
-  # # The jack module.
-  # services.jack = {
-  #   jackd.enable = true;
-  #   # support ALSA only programs via ALSA JACK PCM plugin
-  #   alsa.enable = false;
-  #   # support ALSA only programs via loopback device (supports programs like Steam)
-  #   loopback = {
-  #     enable = true;
-  #   };
-  # };
-
   # Load the sequencer and midi kernel modules.
   boot.kernelModules = ["snd-seq" "snd-rawmidi"];
 
-  users.extraUsers.mindtree.extraGroups = [ "audio" "jackaudio" "realtime" ];
+  users.extraUsers.mindtree.extraGroups = [ "audio" "realtime" ];
 }
