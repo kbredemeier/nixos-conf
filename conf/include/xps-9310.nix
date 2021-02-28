a6c1f6dd90582ad7fbc97c2d080da11ff0219535
mitchmindtree <mail@mitchellnordine.com>
2021-02-28 09:41:17 +0100

    Add nixos-hardware channel and point to it instead of git

--- conf/include/xps-9310.nix
+++ conf/include/xps-9310.nix
@@ -2,30 +2,20 @@
 # `/etc/nixos/configuration.nix`.
 #
 # Made specifically for personal use with the late 2020 Dell XPS 13 9310.
-{ config, pkgs, ... }:
-let
-  # TODO: Switch to a stable nixos-hardware channel once this has landed.
-  # nixos-hardware = /home/mindtree/programming/nix/nixos-hardware;
-  nixos-hardware = builtins.fetchGit {
-    url = "https://github.com/mitchmindtree/nixos-hardware.git";
-    ref = "xps-9310";
-    rev = "f361f64db031dcc13e7d3bfe0c2e939ba220e7aa";
-  };
-in
-  {
-    imports = [
-      ''${nixos-hardware}/dell/xps/13-9310''
-      ../audio.nix
-      ../conf.nix
-      ../gaming.nix
-      ../home-manager.nix
-      ../users.nix
-    ];
+{ config, pkgs, ... }: {
+  imports = [
+    <nixos-hardware/dell/xps/13-9310>
+    ../audio.nix
+    ../conf.nix
+    ../gaming.nix
+    ../home-manager.nix
+    ../users.nix
+  ];
 
-    # Enable Intel's Hybrid Driver.
-    nixpkgs.config.packageOverrides = pkgs: {
-      vaapiIntel = pkgs.vaapiIntel.override {
-        enableHybridCodec = true;
-      };
+  # Enable Intel's Hybrid Driver.
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {
+      enableHybridCodec = true;
     };
-  }
+  };
+}
